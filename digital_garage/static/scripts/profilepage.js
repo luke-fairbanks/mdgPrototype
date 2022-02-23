27605fb2e83f7c2f2e0a6c85b5af181dc244c173
luke <lukedfairbanks@gmail.com>
2022-02-23 14:11:29 -0700

    lil changes

--- digital_garage/static/scripts/profilepage.js
+++ digital_garage/static/scripts/profilepage.js
@@ -25,53 +25,41 @@ window.onclick = function(event) {
     }
 }
 
-//import WalletConnect from "./node_modules/@walletconnect/client"
-//import QRCodeModal from "./node_modules/@walletconnect/qrcode-modal";
+var WalletConnect = window.WalletConnect.default;
+var WalletConnectQRCodeModal = window.WalletConnectQRCodeModal.default
 
-// Create a connector
-const connector = new WalletConnect({
-  bridge: "https://bridge.walletconnect.org", // Required
-  qrcodeModal: QRCodeModal,
+var walletConnector = new WalletConnect({
+    bridge: 'https://bridge.walletconnect.org'
 });
-
-// Check if connection is already established
-if (!connector.connected) {
-  // create new session
-  connector.createSession();
-}
-
-// Subscribe to connection events
-connector.on("connect", (error, payload) => {
-  if (error) {
-    throw error;
-  }
-
-  // Get provided accounts and chainId
-  const { accounts, chainId } = payload.params[0];
-});
-
-connector.on("session_update", (error, payload) => {
-  if (error) {
-    throw error;
-  }
-
-  // Get updated accounts and chainId
-  const { accounts, chainId } = payload.params[0];
-});
-
-connector.on("disconnect", (error, payload) => {
-  if (error) {
-    throw error;
-  }
-
-  // Delete connector
-});
-
-const uri = connector.uri;
-
-
-$(".wallet-connect-btn").click(function(){
-    alert('bru')
-    QRCodeModal.open(uri);
+var uri = walletConnector.uri;
+$('#wallet-connect-btn').on('click',function(){
+    WalletConnectQRCodeModal.open(uri);
 })
 
+
+import bncOnboard from 'https://cdn.skypack.dev/bnc-onboard';
+
+let web3;
+
+var Web3 = window.Web3.default
+//var Onboard = window.bncOnboard.default
+const onboard = bncOnboard({
+  dappId: '23a4ca89-79e5-435b-9dc7-ae5d1914d1b1',       // [String] The API key created by step one above
+  networkId: 1,  // [Integer] The Ethereum network ID your Dapp uses.
+  subscriptions: {
+    wallet: wallet => {
+       web3 = new Web3(wallet.provider)
+    }
+  },
+  walletSelect: {
+      wallets: [
+        { walletName: "coinbase", preferred: true },
+        { walletName: "trust", preferred: true },
+        { walletName: "metamask", preferred: true },
+        { walletName: "authereum" },
+        { walletName: "walletLink", rpcUrl: "wss://mainnet.infura.io/ws/v3/8bc341c8b6b94dd59171587ca949f894", appName: "MDGprototype" }]
+  }
+});
+
+await onboard.walletSelect();
+await onboard.walletCheck();
